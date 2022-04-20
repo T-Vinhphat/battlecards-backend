207ec2617a8541d5be82364af50ce6741d656518
alexis bleuse <bleuse.alexis@gmail.com>
2022-04-20 12:01:57 +0200

    added nodemon, route and changed port for backend

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
+router.get("/game", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/login", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/signin", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/profil/:user", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/shop", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/collection", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/my-collection", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/my-collection/add", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/my-collection/delete", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/my-collection/see", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
 module.exports = router;
